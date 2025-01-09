16445637dfd3b219fbbd858847fb01d543dc2ca3
Clément Lagier <clement.lagier@epitech.eu>
2025-01-09 17:04:59 +0900

    [ADD] Added a ChapterPage page to get the second screen

---
+++ mobile_app/lib/pages/chapter_page.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class ChapterPage extends StatelessWidget {
+  const ChapterPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Chapter Page'),
+      ),
+      body: Center(
+        child: Text('Chapter Page'),
+      ),
+    );
+  }
+}
