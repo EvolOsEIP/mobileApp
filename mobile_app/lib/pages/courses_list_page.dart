c8fe448c830a8a41fb17284e56f2a7458fcb430c
Clément Lagier <clement.lagier@epitech.eu>
2025-01-10 18:51:59 +0900

    [UPDATE] Added the svg images as a suffix of the courses cards

--- mobile_app/lib/pages/courses_list_page.dart
+++ mobile_app/lib/pages/courses_list_page.dart
@@ -1,5 +1,6 @@
 import 'dart:convert';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class CoursesListPage extends StatefulWidget {
   final dynamic chapter;
@@ -22,15 +23,56 @@ class _CoursesListPageState extends State<CoursesListPage> {
       body: ListView.builder(
         itemCount: chapter['courses'].length,
         itemBuilder: (context, index) {
-        return Card(
-          child: ListTile(
-            title: Text(chapter['courses'][index]['title']),
-            subtitle: Text(chapter['courses'][index]['description']),
+          return InkWell(
             onTap: () {
-              Navigator.pushNamed(context, '/course_detail', arguments: chapter['courses'][index]);
+              Navigator.pushNamed(
+                context, '/course_detail',
+                arguments: chapter['courses'][index],
+              );
             },
-          ),
-        );
+
+            child: Card(
+              margin: const EdgeInsets.symmetric(
+                  vertical: 10, horizontal: 16),
+              child: SizedBox(
+                height: 250, // Set the desired height for the cards
+                child: Padding (
+                  padding: const EdgeInsets.all(16.0),
+                  child: Row(
+                    children: [
+                      SvgPicture.asset(
+                        "assets/images/genetic-data-svgrepo-com.svg",
+                        width: 100,
+                        height: 100,
+                      ),
+                      const SizedBox(width: 16),
+                      Expanded(
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Padding(
+                              padding: const EdgeInsets.only(bottom: 8.0),
+                              child: Text(
+                                chapter['courses'][index]['title'],
+                                style: const TextStyle(
+                                  fontSize: 18,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                            ),
+                            Text(
+                              chapter['courses'][index]['description'],
+                              style: const TextStyle(fontSize: 14),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ]
+                  ),
+                ),
+              ),
+            ),
+          );
         },
       ),
     );
