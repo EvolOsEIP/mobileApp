24c45c1cc2db111866520f9303ddb52a227ef5e1
Clément Lagier <clement.lagier@epitech.eu>
2025-01-13 17:15:50 +0900

    [ADD] Add cards as button to the homepage

--- mobile_app/lib/pages/home_page.dart
+++ mobile_app/lib/pages/home_page.dart
@@ -14,26 +14,89 @@ class _HomePageState extends State<HomePage> {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: CustomColors.dark_accent,
-        title: Text(
+        title: const Text(
           'Home Page',
-          style: TextStyle(
-            color: Colors.white
-          ),
+          style: TextStyle(color: Colors.white),
         ),
       ),
-      body: Container(
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
         child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
           children: [
-            Card(
-              child: Text("Item 1"),
+            SizedBox(
+              height: 200,
+              child: Card(
+                elevation: 4,
+                child: InkWell(
+                  onTap: () {
+                    Navigator.pushNamed(context, '/chapters');
+                  },
+                  child: Center(
+                    child: Padding(
+                      padding: const EdgeInsets.all(16.0),
+                      child: Text(
+                        "Voir la liste des chapitres",
+                        textAlign: TextAlign.center,
+                        style: Theme.of(context).textTheme.bodyLarge,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
             ),
+            const SizedBox(height: 16.0),
+            // Row with two cards
             Row(
               children: [
-                Card(
-                  child: Text("Item 2"),
+                // Second card
+                Expanded(
+                  child: SizedBox(
+                    height: 200,
+                    child: Card(
+                      elevation: 4,
+                      child: InkWell(
+                        onTap: () {
+                          // Handle second button tap
+                        },
+                        child: Center(
+                          child: Padding(
+                            padding: const EdgeInsets.all(16.0),
+                            child: Text(
+                              'Profile',
+                              textAlign: TextAlign.center,
+                              style: Theme.of(context).textTheme.bodyLarge,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
                 ),
-                Card(
-                  child: Text("Item 3"),
+                const SizedBox(width: 16.0),
+                // Third card
+                Expanded(
+                  child: SizedBox(
+                    height: 200,
+                    child: Card(
+                      elevation: 4,
+                      child: InkWell(
+                        onTap: () {
+                          // Handle second button tap
+                        },
+                        child: Center(
+                          child: Padding(
+                            padding: const EdgeInsets.all(16.0),
+                            child: Text(
+                              'Ceci n\'est pas un bouton',
+                              textAlign: TextAlign.center,
+                              style: Theme.of(context).textTheme.bodyLarge,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
                 ),
               ],
             ),
