893e333eacdfeaf245c8a8ce3214b392da34102f
Clément Lagier <clement.lagier@epitech.eu>
2025-01-22 02:15:58 +0900

    [UPDATE] Made the units cards responsive

--- mobile_app/lib/pages/unit_list_page.dart
+++ mobile_app/lib/pages/unit_list_page.dart
@@ -45,57 +45,125 @@ class _UnitListPageState extends State<UnitListPage> {
         // AppBar with a title for the page
         title: const Text('Liste des modules'),
       ),
+
       body: data == null
           ? const Center(
-              // Shows a loading spinner while the data is being fetched
-              child: CircularProgressIndicator(),
+              child: CircularProgressIndicator(), // Show a loader if data is null.
             )
           : ListView.builder(
-              // Builds a scrollable list of unit cards
-              itemCount: data["units"].length, // Number of units in the data
+              // Dynamically builds a list of units  based on the number of items.
+              itemCount: data["units"].length, // Total number of units.
               itemBuilder: (context, index) {
                 return InkWell(
                   onTap: () {
-                    // Navigates to the chapters page, passing the selected unit data
+                    // Navigate to the courses page, passing the selected chapter as an argument.
                     Navigator.pushNamed(context, '/chapters',
                         arguments: data["units"][index]);
                   },
                   child: Card(
-                    color: CustomColors.accent, // Custom background color for the card
+                    color: CustomColors.accent, // Use custom accent color for the card.
                     margin: const EdgeInsets.symmetric(
-                        vertical: 10, horizontal: 16), // Adds margin around each card
+                        vertical: 10, horizontal: 16), // Card spacing.
                     child: SizedBox(
-                      height: 250, // Sets a fixed height for each card
+                      height: 250, // Set a fixed height for the cards.
                       child: Padding(
-                        padding: const EdgeInsets.all(16.0), // Adds padding inside the card
+                        padding: const EdgeInsets.all(16.0), // Uniform padding for the card content.
                         child: Row(
                           children: [
-                            // Displays an SVG icon for each unit
-                            SvgPicture.asset(
-                              "assets/images/genetic-data-svgrepo-com.svg",
-                              width: 100,
-                              height: 100,
+                            Column(
+                              mainAxisAlignment: MainAxisAlignment.start,
+                              children: [
+                                SvgPicture.asset(
+                                  "assets/images/genetic-data-svgrepo-com.svg",
+                                  width: 100, // Image width.
+                                  height: 100, // Image height.
+                                ),
+                                const SizedBox(height: 16),
+                                // Add three stars
+                                Row(
+                                  children: const [
+                                    Icon(
+                                      Icons.star,
+                                      color: Colors.yellow,
+                                    ),
+                                    Icon(
+                                      Icons.star,
+                                      color: Colors.yellow,
+                                    ),
+                                    Icon(
+                                      Icons.star,
+                                      color: Colors.yellow,
+                                    ),
+                                  ],
+                                ),
+                              ],
                             ),
-                            const SizedBox(width: 16), // Adds spacing between the image and text
+                            const SizedBox(width: 16), // Space between the image and text.
                             Expanded(
                               child: Column(
-                                crossAxisAlignment: CrossAxisAlignment.start, // Aligns text to the start
+                                crossAxisAlignment: CrossAxisAlignment.start, // Align text to the start.
                                 children: [
-                                  // Displays the unit title in bold
-                                  Padding(
-                                    padding: const EdgeInsets.only(bottom: 8.0),
-                                    child: Text(
-                                      data["units"][index]['title'],
-                                      style: const TextStyle(
-                                        fontSize: 18,
-                                        fontWeight: FontWeight.bold,
+                                  Row(
+                                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                    crossAxisAlignment: CrossAxisAlignment.start,
+                                    children: [
+                                      Expanded(
+                                        child: Text(
+                                          data["units"][index]['title'], // Unit title.
+                                          style: const TextStyle(
+                                            fontSize: 18, // Font size for the title.
+                                            fontWeight: FontWeight.bold, // Bold font for emphasis.
+                                          ),
+                                          softWrap: true, // Allow text to wrap to the next line if it exceeds the width.
+                                          overflow: TextOverflow.ellipsis, // Ellipsis for overflow text.
+                                          maxLines: 2, // Maximum of 2 lines for the title.
+                                        ),
                                       ),
-                                    ),
+                                      const Icon(
+                                        Icons.arrow_forward_ios,
+                                        color: Colors.black,
+                                      ),
+                                    ],
                                   ),
-                                  // Displays the unit description in regular text
+                                  // Chapter description text.
                                   Text(
-                                    data["units"][index]["description"],
-                                    style: const TextStyle(fontSize: 14),
+                                    data["units"][index]['description'],
+                                    style: const TextStyle(
+                                      fontSize: 14
+                                    ), // Smaller font size for description.
+                                    overflow: TextOverflow.ellipsis, // Ellipsis for overflow text.
+                                    maxLines: 5, // Maximum of 3 lines for the description.
+                                  ),
+                                  const Spacer(),
+                                  // Add text for the status of the chapter ("En cours", "Terminé" or "À venir"), and the number of courses completed
+                                  Row(
+                                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                    children: [
+                                      Row(
+                                        children: [
+                                          const Text(
+                                            "Status: ",
+                                            style: TextStyle(
+                                              fontSize: 14,
+                                              fontWeight: FontWeight.w600,
+                                            ),
+                                          ),
+                                          Text(
+                                            data["units"][index]['status'],
+                                            style: const TextStyle(
+                                              fontSize: 14,
+                                            ),
+                                          ),
+                                        ],
+                                      ),
+                                      Text(
+                                        "${data['units'][index]['completedChapters']} / ${data['units'][index]['totalChapters']}",
+                                        style: const TextStyle(
+                                          fontSize: 14,
+                                          fontWeight: FontWeight.w600,
+                                        ),
+                                      ),
+                                    ],
                                   ),
                                 ],
                               ),
