f90898c58d809f04a826f611a76e02a8ed4829db
apolline <apolline.fontaine@epitech.eu>
2025-01-22 13:51:30 +0100

    TEST - add unit test for generateErrorMessage method

--- mobile_app/lib/pages/course_page.dart
+++ mobile_app/lib/pages/course_page.dart
@@ -1,6 +1,7 @@
 import 'dart:async';
 import 'package:flutter/material.dart';
 import '../utils/assistant.dart';
+import 'course_page_utils.dart';
 
 /// This part is used to load courses content.
 ///
@@ -133,7 +134,7 @@ class _CoursePageState extends State<CoursePage> {
       });
     } else {
       setState(() {
-        errorMessage = _generateErrorMessage(userResponse, expectedResponse);
+        errorMessage = ErrorUtils.generateErrorMessage(userResponse, expectedResponse);
       });
     }
   }
@@ -157,38 +158,6 @@ class _CoursePageState extends State<CoursePage> {
         ],
       ),
     );
-  }
-
-  /// Generates and display a specific error message based on the user's response.
-  ///
-  /// If the answer doesn't match with the expected one, we try to help the user.
-  /// We check what is wrong and if we know we inform the user.
-  /// Error handling :
-  /// - Missing or Too many capital(s).
-  /// - Missing a point at the end of the sentence.
-  /// - Missing or Too many space(s).
-  ///
-  /// Returns :
-  /// - A error message string.
-  String _generateErrorMessage(String userResponse, String expectedResponse) {
-    if (userResponse.isEmpty) {
-      return 'Le champ de saisie est vide. Veuillez entrer une réponse.';
-    }
-
-    if (userResponse.toLowerCase() == expectedResponse.toLowerCase()) {
-      return 'Vérifiez la majuscule: il pourrait y avoir des majuscules en trop ou manquants.';
-    }
-
-    if (userResponse.replaceAll(' ', '') ==
-        expectedResponse.replaceAll(' ', '')) {
-      return 'Vérifiez les espaces : il pourrait y avoir des espaces en trop ou manquants.';
-    }
-
-    if (!userResponse.endsWith('.') && expectedResponse.endsWith('.')) {
-      return 'Votre réponse manque un point à la fin.';
-    }
-
-    return 'Votre réponse est incorrecte. Veuillez revoir l’instruction.';
   }
 
   /// Builds the UI for the course page.
--- mobile_app/test/course_page_test.dart
+++ mobile_app/test/course_page_test.dart
@@ -1,7 +1,52 @@
-import 'package:mobile_app/pages/course_page.dart';
-import 'package:flutter/material.dart';
+import 'package:mobile_app/pages/course_page_utils.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
+  group('Test error messages from generateErrorMessage', () {
+    test('Empty user response', () {
+      expect(ErrorUtils.generateErrorMessage('', 'Something'), 'Le champ de saisie est vide. Veuillez entrer une réponse.');
+    });
 
+    test('Missing capital', () {
+      expect(
+        ErrorUtils.generateErrorMessage('hello world.', 'Hello World.'),
+        'Vérifiez la majuscule: il pourrait y avoir des majuscules en trop ou manquants.',
+      );
+    });
+
+    test('Extra capital', () {
+      expect(
+        ErrorUtils.generateErrorMessage('Hello World.', 'hello World.'),
+        'Vérifiez la majuscule: il pourrait y avoir des majuscules en trop ou manquants.',
+      );
+    });
+
+    test('Too much space', () {
+      expect(
+        ErrorUtils.generateErrorMessage('Hello   World.', 'Hello World.'),
+        'Vérifiez les espaces : il pourrait y avoir des espaces en trop ou manquants.',
+      );
+    });
+
+    test('Missing space', () {
+      expect(
+        ErrorUtils.generateErrorMessage('HelloWorld.', 'Hello World.'),
+        'Vérifiez les espaces : il pourrait y avoir des espaces en trop ou manquants.',
+      );
+    });
+
+    test('Missing period', () {
+      expect(
+        ErrorUtils.generateErrorMessage('Hello World', 'Hello World.'),
+        'Votre réponse manque un point à la fin.',
+      );
+    });
+
+    test('Other case not registered', () {
+      expect(
+        ErrorUtils.generateErrorMessage('Hallo wayayay.', 'Hello World.'),
+        'Votre réponse est incorrecte. Veuillez revoir l’instruction.',
+      );
+    });
+  });
 }
