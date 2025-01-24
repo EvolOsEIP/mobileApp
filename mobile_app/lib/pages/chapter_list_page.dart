22c651d83458bd58558bab3f2e0e4e6882b16455
Clément Lagier <clement.lagier@epitech.eu>
2025-01-25 01:16:52 +0900

    [UPDATE] Send a request to get the list of chapters instead of just reading a json file

--- mobile_app/lib/pages/chapter_list_page.dart
+++ mobile_app/lib/pages/chapter_list_page.dart
@@ -1,13 +1,15 @@
 import 'dart:convert'; // Provides JSON encoding and decoding utilities.
 import 'package:flutter/material.dart'; // Flutter framework for UI components.
 import 'package:flutter_svg/flutter_svg.dart'; // Library to handle SVG images.
+import 'package:http/http.dart' as http; // Library for making HTTP requests.
+import 'package:flutter_dotenv/flutter_dotenv.dart'; // Library to read configuration from a .env file.
 
 import 'package:mobile_app/utils/colors.dart'; // Custom color definitions for the app.
 
+
 /// A StatefulWidget to display a list of chapters in a unit.
 class ChapterListPage extends StatefulWidget {
-  final dynamic units; // Data containing information about units and chapters.
-  const ChapterListPage({super.key, required this.units});
+  const ChapterListPage({super.key});
 
   @override
   _ChapterListPageState createState() => _ChapterListPageState();
@@ -15,11 +17,42 @@ class ChapterListPage extends StatefulWidget {
 
 /// The state class for ChapterListPage, responsible for rendering the UI.
 class _ChapterListPageState extends State<ChapterListPage> {
+  dynamic units = null; // Variable to store the list of chapters.
+
+
+  /// Fetches the list of chapters from the API.
+  Future<void> _fetchUnits() async {
+    try {
+      final response = await http.get(
+        Uri.parse(dotenv.env['API_URL'].toString() + '/chapters'),
+          headers: <String, String>{
+            'Content-Type': 'application/json; charset=UTF-8',
+          },
+        );
+      if (response.statusCode == 200) {
+        setState(() {
+          units = jsonDecode(response.body);
+        });
+      } else {
+        setState(() {
+          units = null;
+        });
+        print('Failed to load chapters');
+      }
+    } catch (e) {
+      print('Failed to load chapters');
+      print(e);
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _fetchUnits();
+  }
   @override
   Widget build(BuildContext context) {
-    // Retrieve the passed 'units' argument from the navigation route.
-    final units = ModalRoute.of(context)!.settings.arguments as dynamic;
-
+    print(dotenv.env['API_URL']);
     return Scaffold(
       appBar: AppBar(
         elevation: 0, // Removes the shadow under the AppBar.
@@ -37,13 +70,13 @@ class _ChapterListPageState extends State<ChapterListPage> {
             )
           : ListView.builder(
               // Dynamically builds a list of chapters based on the number of items.
-              itemCount: units["chapters"].length, // Total number of chapters.
+              itemCount: units.length, // Total number of chapters.
               itemBuilder: (context, index) {
                 return InkWell(
                   onTap: () {
                     // Navigate to the courses page, passing the selected chapter as an argument.
-                    Navigator.pushNamed(context, '/courses',
-                        arguments: units["chapters"][index]);
+                   // Navigator.pushNamed(context, '/courses',
+                   //     arguments: units[index]);
                   },
                   child: Card(
                     color: CustomColors.accent, // Use custom accent color for the card.
@@ -94,7 +127,7 @@ class _ChapterListPageState extends State<ChapterListPage> {
                                     children: [
                                       Expanded(
                                         child: Text(
-                                          units["chapters"][index]['title'], // Chapter title.
+                                          units[index]['title'], // Chapter title.
                                           style: const TextStyle(
                                             fontSize: 18, // Font size for the title.
                                             fontWeight: FontWeight.bold, // Bold font for emphasis.
@@ -112,7 +145,7 @@ class _ChapterListPageState extends State<ChapterListPage> {
                                   ),
                                   // Chapter description text.
                                   Text(
-                                    units["chapters"][index]['description'],
+                                    units[index]['description'],
                                     style: const TextStyle(
                                       fontSize: 14
                                     ), // Smaller font size for description.
@@ -134,7 +167,8 @@ class _ChapterListPageState extends State<ChapterListPage> {
                                             ),
                                           ),
                                           Text(
-                                            units["chapters"][index]['status'],
+                                            //units[index]['status'],
+                                            "En cours",
                                             style: const TextStyle(
                                               fontSize: 14,
                                             ),
@@ -142,7 +176,7 @@ class _ChapterListPageState extends State<ChapterListPage> {
                                         ],
                                       ),
                                       Text(
-                                        "${units["chapters"][index]['completedCourses']} / ${units["chapters"][index]['totalCourses']}",
+                                        "${units[index]['completedCourses']} / ${units[index]['totalCourses']}",
                                         style: const TextStyle(
                                           fontSize: 14,
                                           fontWeight: FontWeight.w600,
