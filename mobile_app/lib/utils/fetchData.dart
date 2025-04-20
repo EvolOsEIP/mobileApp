baa650d9e8bce2549e9f267e8bbeaaa4cc9dd06e
nourrysebastieN <sebastien.nourr@epitech.eu>
2025-04-21 01:18:02 +0700

    [ADD] login and register pages done, caching still to be implemented

--- mobile_app/lib/utils/fetchData.dart
+++ mobile_app/lib/utils/fetchData.dart
@@ -13,10 +13,31 @@ Future<List<dynamic>> fetchFromJson(String filePath) async {
   }
 }
 
-Future<List<dynamic>> fetchFromApi(String endpoint, {Map<String, String>? headers}) async {
+Future<List<dynamic>> fetchFromApi(String endpoint,
+    {Map<String, String>? headers}) async {
   var url = Uri.http(dotenv.env["HOST_URL"].toString(), endpoint);
   try {
-    final response = await http.get(url, headers: headers).timeout(const Duration(seconds: 5));
+    final response = await http
+        .get(url, headers: headers)
+        .timeout(const Duration(seconds: 5));
+    print(response.body);
+    if (response.statusCode == 200) {
+      return jsonDecode(response.body);
+    } else {
+      throw Exception('Failed to load data');
+    }
+  } catch (e) {
+    print('Error: $e');
+    return [];
+  }
+}
+
+Future<List<dynamic>> postToApi(String endpoint, Object? body) async {
+  var url = Uri.http(dotenv.env["HOST_URL"].toString(), endpoint);
+  try {
+    final response =
+        await http.post(url, body: body).timeout(const Duration(seconds: 5));
+    print(response.body);
     if (response.statusCode == 200) {
       return jsonDecode(response.body);
     } else {
